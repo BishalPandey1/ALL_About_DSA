685515526f5b25d50efb2ae6a25a44c1a4d656a2
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2024-04-21 21:38:30 +0545

    Add files via upload

---
+++ Deleting last node in link list.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *link;
+};
+
+void del_last(struct node **head) {
+    if(*head == NULL) {
+        printf("List is empty\n");
+    } else if((*head)->link == NULL) {
+        free(*head);
+        *head = NULL;
+    } else {
+        struct node *temp = *head;
+        while(temp->link->link != NULL) {
+            temp = temp->link;
+        }
+        free(temp->link);
+        temp->link = NULL;
+    }
+}
+
+int main() {
+    struct node *head = NULL; // Assuming head is initialized properly
+    del_last(&head);
+    struct node *ptr = head;
+    while(ptr != NULL) {
+        printf("%d ", ptr->data);
+        ptr = ptr->link;
+    }
+    return 0;
+}
